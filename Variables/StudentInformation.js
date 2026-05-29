587cb453c6b3d35d18731d7fc3ae5c05056e4cce
Noothi Srivarshini <srivarshini587@gmail.com>
2026-05-29 13:23:34 +0530

    Created a StudentInformation in js

---
+++ Variables/StudentInformation.js
@@ -0,0 +1,13 @@
+//Student details
+
+let Studentname = "Srivarshini";
+let age = 23;
+let course = "BTech";
+let college = "Malla Reddy Engineering College For Women";
+let skills = "HTML,CSS,JS";
+
+console.log(Studentname);
+console.log(age);
+console.log(course);
+console.log(college);
+console.log(skills);
