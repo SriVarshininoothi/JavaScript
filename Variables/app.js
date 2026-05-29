62c0eeaad29de0cf2a3cabc17a1cb78e1cda96b8
Noothi Srivarshini <srivarshini587@gmail.com>
2026-05-29 13:16:34 +0530

    Updated with variables and datatypes

--- Variables/app.js
+++ Variables/app.js
@@ -1,1 +1,36 @@
 console.log("Js");
+
+// let, var, const
+//let - changing Values
+
+let fruits = "apple";
+console.log(fruits);
+console.log(typeof(fruits));
+
+
+let a = 100;
+ a = 80;
+console.log(a);
+
+var b = 90;
+b=100;
+console.log(b);
+
+var c= 8;
+var d = 9;
+console.log(d);
+
+
+//Datatypes
+let name="shinchan";
+let age = 21;
+let isstudent = true;
+let s;
+
+console.log(name);
+console.log(age);
+console.log(isstudent);
+
+console.log(s);
+console.error("Error found");
+console.warn("Welcome");
