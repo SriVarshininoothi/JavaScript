c40532d7c47001b04f0d526f6c853af6dcaba1ef
Noothi Srivarshini <srivarshini587@gmail.com>
2026-05-29 13:45:19 +0530

    Created a Datatypes and variables in js

---
+++ Variables/Datatypes.js
@@ -0,0 +1,79 @@
+let language = "Js";
+let bornin = 1995;
+let father = "Brendar Eich";
+
+console.log("language:", language, typeof language);
+console.log("bornin:", bornin, typeof bornin);
+console.log("father:", father, typeof father);
+
+//String 
+
+let FullName = "Shinchan";
+let city = "Novida";
+let course = "JavaScript";
+
+console.log("FullName:", FullName, typeof FullName);
+console.log("city:", city, typeof city);
+console.log("course:", course, typeof course);
+
+//Number
+let ageis = 23;
+let marks= 89;
+
+console.log("ageis:", ageis, typeof ageis);
+console.log("marks:", marks, typeof marks);
+
+
+//Boolean
+let isStudent = true;
+let isEmployed = false;
+console.log("isStudent:", isStudent, typeof isStudent);
+console.log("isEmployed:", isEmployed, typeof isEmployed);
+
+//Undefined
+let address;
+let mobile;
+
+console.log("address:", address, typeof address);
+console.log("mobile:", mobile, typeof mobile);
+
+//Null
+let middlename= null;
+let street= null;
+console.log("middlename:", middlename, typeof middlename);
+console.log("street:", street, typeof street);
+
+
+//Array
+let colors =["Purple","Green","Black"];
+let score=[110,230,340];
+console.log("colors:", colors, typeof colors);
+console.log("score:", score, typeof score);
+
+//Object
+let Student= {
+    name:'Shinchan',
+    id:201
+
+};
+console.log("Student:", Student, typeof Student);
+
+//BigInt
+let population = 38665000;
+console.log("population:", population, typeof population);
+//Symbol
+let uniqueid = Symbol("id");
+
+console.log("uniqueid:", uniqueid, typeof uniqueid);
+//const
+const pi = 3.14159265359;
+console.log("pi:", pi, typeof pi);
+//var
+var f= 60;
+f=80;
+console.log("f :",f,typeof(f));
+
+
+
+
+
