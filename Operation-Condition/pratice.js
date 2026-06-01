6e97e11359911e29b687e7738694e16772e15cbb
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-01 21:38:05 +0530

    created a pratice js

---
+++ Operation-Condition/pratice.js
@@ -0,0 +1,72 @@
+// operaton
+
+let age = 18;
+if (age >= 18) {
+    console.log("Ready to vote");
+}
+else {
+    console.log("Not Ready");
+}
+
+//assignment
+
+let a = 10;
+let b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+
+//comparsion
+
+let c = 30;
+let d = 40;
+
+console.log(a == b);
+console.log(a == b);
+console.log(a > b);
+console.log(a < b);
+console.log(a != b);
+console.log(a <= b);
+console.log(a >= b);
+
+
+//logical
+
+let p = 20;
+let q = 50;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+console.log(!b);
+
+
+//switch
+
+let day = 4;
+
+switch (day) {
+
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    default:
+         console.log("Not found");
+        
+}
