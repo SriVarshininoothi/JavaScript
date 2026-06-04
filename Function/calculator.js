fa3463f093b563059ee6a8c5c35ad63626da2338
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-04 21:48:50 +0530

    Calculator created

---
+++ Function/calculator.js
@@ -0,0 +1,34 @@
+//add
+
+function add(a,b){
+    return a+b;
+}
+let res =add(10,5);
+console.log(res);
+
+
+//sub
+
+function sub(a,b){
+    return a-b;
+}
+let s = sub(30,20);
+console.log(s);
+
+
+//mul
+
+function mul(a,b){
+    return a*b;
+}
+let m = mul(2,6);
+console.log(m);
+
+
+//div
+
+function div(a,b){
+    return a/b;
+}
+let d  = div(12,4);
+console.log(d);
