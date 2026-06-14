8f374f8f21d60568222f1fac645a4de4bf5ce536
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-14 11:53:07 +0530

    Created a String methods

---
+++ StringMethods_Regex/app.js
@@ -0,0 +1,36 @@
+
+//string
+
+let name="Rahul";
+let email = "rahul@gmail.com";
+
+console.log(email);
+
+console.log(name.length);
+
+
+//include
+let skill = "Javascript";
+console.log(skill.includes("Java"));
+console.log(email.includes("@"));
+
+//split()
+
+let fruits = "Apple,Mango,Orange,Grapes";
+
+fruits.split(",");
+console.log(fruits);
+
+//replace
+
+let  n = skill.replace("Javascript","Java");
+
+console.log(n);
+
+
+//trim
+
+let product = " Dell ";
+console.log(product.length);
+console.log(product.trim().length);
+
