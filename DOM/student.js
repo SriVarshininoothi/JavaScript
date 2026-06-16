944da305f3c54df484ea294c8b954c975564e4e3
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-16 16:14:45 +0530

    Created student information

---
+++ DOM/student.js
@@ -0,0 +1,10 @@
+let heading = document.getElementById("heading");
+let para = document.getElementById("para");
+
+function view() {
+    heading.innerText = "Student Introduction";
+    heading.style.color = "purple";
+
+    para.innerText = "Hello! My name is Rahul. I am a student who is interested in learning programming and web development.";
+    para.style.color = "blue";
+}
