61ae56ee1ed6e1521a709fd485bdc8f7396c35be
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-16 07:40:38 +0530

    created getElementById() and styles to heading

---
+++ DOM/app.js
@@ -0,0 +1,14 @@
+
+let heading = document.getElementById("title");
+
+heading.innerHTML = "Welcome to JavaScript";
+heading.style.color = "white"
+heading.style.backgroundColor="purple"
+heading.style.fontSize="40px"
+
+
+let btn = document.getElementById("btn");
+function change(){
+    heading.innerHTML="This are topics";
+    btn.style.color="Green";
+}
