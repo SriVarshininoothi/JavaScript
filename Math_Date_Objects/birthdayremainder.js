f0a956d81337b52f95ab81630f63878672953840
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-16 07:23:22 +0530

    created a birthdayremainder

--- Math_Date_Objects/birthdayremainder.js
+++ Math_Date_Objects/birthdayremainder.js
@@ -1,7 +1,15 @@
 
 //birthday remainder
 
-let birthday = "2002-10-18";
+let birthday = new Date("2026-10-18"); // birthday date
 
-let birthdaydate = new Date(birthday);
-let currentDate = new Date();
+let currentDate = new Date(); //today date
+
+let milisecondsRemaining =  birthday-currentDate;  //js convert them into miliseconds
+
+let daysRemaining = Math.ceil(milisecondsRemaining/(1000*60*60*24)); //convert miliseconds into hours 
+// 1000ms * 60 sec * 60 min * 24 hours  
+
+console.log("Birthday:",birthday.toDateString()); // Convert Date to String 
+console.log("Today:",currentDate.toDateString());
+console.log("Days Remainig: ",daysRemaining);
