9251cc3d1399981b367e77511e86dfc50baed1b0
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-19 11:25:59 +0530

    Upadted with counter

--- DOM-Event-Handlers/app.js
+++ DOM-Event-Handlers/app.js
@@ -2,9 +2,9 @@
 const box = document.getElementById("container");
 
 box.innerHTML = "<h4> Enter Element </h4>"
-//box.textContent = "<h6> Enter Element </h6>"
+box.textContent = "<h6> Enter Element </h6>"
 
-
+//dynamic
 let product = {
     name:"HP Laptop",
     price : 45000,
@@ -14,16 +14,21 @@ let product = {
 }
 
 
-// box.innerHTML = `<h2> ${product.name}</h2>`
-// box.innerHTML = `<h3> ${product.address.city} </h3>`
-// // box.textContent = "Hello world"
+box.innerHTML = `<h2> ${product.name}</h2>`
+box.innerHTML = `<h3> ${product.address.city} </h3>`
+box.textContent = "Hello world"
 
-// const title = document.getElementById("title");
+//Style Manipulation
 
-// title.style.color="red";
-// title.style.backgroundColor = "black";
-// title.style.padding ="20px";
+const title = document.getElementById("title");
 
+title.style.color="pink";
+title.style.backgroundColor = "purple";
+title.style.padding ="20px";
+
+//classlist
+
+document.body.classList.add("white");
 
 box.classList.add("active");
 box.classList.remove("active");
@@ -31,18 +36,20 @@ box.classList.remove("active");
 box.classList.toggle("active"); //switches bg color
 box.classList.contains("active");
 
+//event
+
 const inputF = document.getElementById("text");
 inputF.oninput = function(event) {
     console.log("User name : "+event.target.value)
 
 }
 
-inputF.addEventListener("keydown", () => {
+inputF.addEventListener("keyup", (event) => {
     let currentvalue = inputF.value.trim();
     if(currentvalue === ""){
     alert("Enter value");
     }else{
-        inputF.textContent="Success"
+        inputF.value= event.target.value;
     }
 
 });
@@ -52,6 +59,7 @@ const button = document.getElementById("btn");
 button.onclick= function() {
     alert("Hello");
 }
+
 
 
 button.onmouseover=function(){
@@ -67,6 +75,7 @@ console.log("Clicked");
 });
 
 
+
 const btn = document.getElementById("themeBtn");
 
 btn.addEventListener("click", function(){
@@ -74,21 +83,43 @@ document.body.classList.toggle("dark");
 });
 
 
+
 //counter
-let count =0;
+
+let count = 0;
 const display = document.getElementById("counter");
+const increment = document.getElementById("increment")
+const decrement = document.getElementById("decrement");
+const reset = document.getElementById("reset");
+
 
 display.addEventListener("click", () => {
-    count++;
     display.textContent=count;
+
 });
 
 
-const reset = document.getElementById("reset");
+increment.addEventListener("click",() =>{
+    count++;
+    display.textContent=count;
+    display.style.color="Green";
+    display.style.fontSize="20px";
+    display.style.fontWeight="bold";
+});
+
+decrement.addEventListener("click",() =>{
+    count--;
+    display.textContent=count;
+    display.style.color="Red";
+    display.style.fontSize="20px";
+    display.style.fontWeight="bold";
+})
+
+
 
 reset.addEventListener("click", () => {
-
     count=0;
+    display.style.color="black";
     display.textContent=count;
 });
 
