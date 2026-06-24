95e0284711bbc96b4d46e2a3ef6c38139698ac87
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-24 18:08:40 +0530

    updated with settimeout and set interval

---
+++ Times_localStorage/app.js
@@ -0,0 +1,45 @@
+//time
+
+// setTimeout(function(){
+//     console.log("Welcome Students");
+// },2000);
+
+
+let button = document.getElementById("btn");
+let message = document.getElementById("message");
+
+
+button.addEventListener("click", function () {
+
+    setTimeout(function () {
+        message.textContent = "Hello Js";
+
+    }, 3000);
+    message.textContent = "Welcome";
+    console.log("Hi");
+
+});
+
+
+
+// setInterval(function(){
+//     console.log("Running...");
+// },1000);
+
+
+let timer = setInterval(function () {
+    console.log("Running");
+}, 1000);
+
+setTimeout(function () {
+    clearInterval(timer);
+    console.log("Stop");
+
+}, 5000);
+
+//auto
+
+let count=0;
+
+
+
