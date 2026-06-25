3cdcef8d15249187572d198f2d002165aee5f5ce
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-25 19:39:40 +0530

    created callback()

---
+++ Callbacks_Errorhandling/app.js
@@ -0,0 +1,74 @@
+
+//call backs
+
+function greet() { // callback function
+    console.log("hello");
+}
+
+function execute(callback) { //higher-order function
+    callback();
+}
+
+execute(greet);
+
+
+
+
+function wish() {
+    console.log("Happy Birthday!");
+}
+
+setTimeout(function () {
+
+    wish();
+
+}, 1000);
+
+ //Dom events and callbacks
+
+ const button = document.getElementById("btn");
+
+ button.addEventListener("click", function(){
+
+    document.getElementById("result")
+    .textContent = "Button Clicked";
+
+ });
+
+
+ //callstack
+
+ console.log("A");
+ console.log("B");
+ console.log("C");
+
+
+ //Web APIs and  callback queue
+
+ console.log("Start");
+
+ setTimeout(() => {
+
+    console.log("Timer");
+    
+ }, 3000);
+
+ console.log("End");
+
+
+ //event loop and microtask queue
+
+ console.log("Start");
+
+ setTimeout(() => {
+    console.log("timer");
+ },0);
+
+ Promise.resolve()
+ .then(() => {
+    console.log("Promise");
+
+ });
+ console.log("End");
+
+ 
