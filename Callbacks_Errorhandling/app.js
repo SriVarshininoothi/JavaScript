f0498d2dd96910bd2a7c9f223872d2c8ebdfb0dc
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-25 20:20:14 +0530

    created priority

--- Callbacks_Errorhandling/app.js
+++ Callbacks_Errorhandling/app.js
@@ -64,8 +64,7 @@ setTimeout(() => {
     console.log("timer");
 }, 0);
 
-Promise.resolve()
-    .then(() => {
+Promise.resolve().then(() => {
         console.log("Promise");
 
     });
@@ -80,17 +79,39 @@ try {
     console.log(userName);
 } catch (error) {
     console.log("User not found");
-}
-
-
-//finally
-finally {
+} finally {
     console.log("Always Run");
 }
 
 
 //throw
 
-if (age < 18) {
-    throw new Error("Not Eligible");
-}
+// if (age < 18) {
+//     throw new Error("Not Eligible");
+// }
+
+// priority
+let testBtn = document.getElementById("testBtn");
+let priorityResult = document.getElementById("priorityResult"); 
+
+testBtn.addEventListener("click", () => {
+    // Clear the placeholder "Waiting..." on click
+    priorityResult.innerHTML = "";
+   
+
+    // 1. Synchronous Code (Runs immediately)
+    priorityResult.innerHTML += "<div style='color:red;'>1. Normal code ran first!</div>";
+    
+    
+
+    // 2. Promise / Microtask (Runs second)
+    Promise.resolve().then(() => {
+        priorityResult.innerHTML += "<div style ='color:blue;'>2. Promise ran second!</div>";
+         priorityResult.style.color ="Red";
+    });
+
+    // 3. setTimeout / Macrotask (Runs last)
+    setTimeout(() => {
+        priorityResult.innerHTML += "<div style='color:green'>3. setTimeout ran last!</div>";
+    }, 0);
+});
