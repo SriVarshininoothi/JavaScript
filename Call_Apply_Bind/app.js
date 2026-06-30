d7a1a7a73c47d1218cb640a657794c52e4b9e09a
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-30 21:27:17 +0530

    updated with bind()

--- Call_Apply_Bind/app.js
+++ Call_Apply_Bind/app.js
@@ -54,4 +54,105 @@ function introduce(city){
 introduce.call(student,"Hyderabad")
 
 
-//apply()
+//apply() = Same Bike,Different Packing
+//call() = One by One
+//apply() = Whole bag
+
+// const stu = {name:"Kalyani"};
+// let info = ["Hyderabad","Developer"];
+
+
+// function introduce(city,role){
+//     console.log(this.name+" "+ city+" "+role);
+
+// }
+// introduce.apply(stu,info)
+
+const fruits ={
+
+
+    name:"Orange"
+};
+
+function snacks(color,kgs,tr){
+    console.log(this.name +" "+ color +" "+ kgs+" "+tr);
+}
+
+snacks.call(fruits,"Orange",3,4);
+
+
+
+//bind() = Borrow and Use Later
+
+const person1 = {
+    name:"Shinchan"
+};
+
+const person2 ={
+
+    name:"Kazama"
+};
+
+
+function wish(){
+
+    console.log(" Hello " + this.name);
+
+}
+const value = wish.bind(person1);
+value()
+
+
+
+
+//btn
+const btn = document.getElementById("btn");
+
+const user = {
+
+    name:"Sri",
+    showName(){
+        alert(this.name);
+    }
+};
+
+// btn.addEventListener("click",user.showName);
+
+btn.addEventListener("click", 
+    user.showName.bind(user)
+);
+
+const empl1 = {
+
+    name:"Rahul",
+    role:"Developer"
+};
+
+const empl2= {
+
+    name:"varun",
+    role:"Tester"
+}
+
+function details(company){
+    console.log(
+        this.name + " "+ this.role +" "+ company
+    );
+
+}
+
+//apply
+
+
+
+details.call(empl1);
+details.call(empl2);
+
+let infom = ["xyz"];
+
+details.apply(empl1,infom);
+
+//bind
+
+const rahulDetails =  details.bind(empl1,"Netflix");
+rahulDetails();
