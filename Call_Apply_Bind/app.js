4479c9d91e544b4f12e1542bca452ab03e42972f
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-30 15:04:44 +0530

    updated with call()

--- Call_Apply_Bind/app.js
+++ Call_Apply_Bind/app.js
@@ -1,13 +1,57 @@
 
-const person = {
+// const person = {
 
-    name:"varshini",
-    greet() {
-        console.log(this);
-    }
+//     name:"varshini",
+//     greet() {
+//         console.log(this);
+//     }
+
+// }
+// person.greet();
+
+
+
+// //2 same objects
+
+// const person1 = {
+//     name:"harinath"
+// }
+
+// const person2 = {
+//     name:"Kalyan"
+
+// }
+
+// function greet(){
+
+//     console.log("  Good morning " + this.name);
+
+
+//   }
+// greet()
+
+
+const emp1 = {
+    name: "Kiranmai"
 
 }
-person.greet();
+
+function greet(city,country){
+
+    console.log(this.name,city,country);
+
+}
+greet.call(emp1,"khammam","India");
+
+//call() = Borrow and use now
+
+const student = {name:"Aruna"};
+
+function introduce(city){
+    console.log(this.name+" from "+city);
+}
+
+introduce.call(student,"Hyderabad")
 
 
-
+//apply()
