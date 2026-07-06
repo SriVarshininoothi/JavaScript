ef821a3ea689a72a6064b1732335cdd2b9a8679e
SriVarshininoothi <noothi.srivarshini982@gmail.com>
2026-07-06 17:15:40 +0530

    Updated a Gadgets Data

--- Array_Methods/app.js
+++ Array_Methods/app.js
@@ -1,5 +1,4 @@
 
-
 //map
 let pratice = [100,200,300];
 
@@ -12,8 +11,9 @@ console.log(gst);
 
 //products
 
-const products = [{name:"Laptop",price:5000},
-    {name:"phone",price:2000}
+const products = [
+    {name:"Laptop",price:50000},
+    {name:"phone",price:20000}
 ];
 const names = products.map(
     product => product.name
@@ -56,3 +56,39 @@ console.log(total);
 const g = products
         .reduce((sum,product) => sum+product.price,0);
 console.log(g); 
+
+
+//Gadgets Data
+
+const gadgets = [
+
+    {name:"Laptop",price:50000},
+    {name:"tablet",price:30000},
+    {name:"phone",price:20000},
+    {name:"earpods",price:8000},
+    {name:"speaker",price:4000},
+    {name:"Wireless Mouse",price:1000},
+    {name:"headset",price:2000}
+];
+
+
+//map
+
+const discounts = gadgets
+        .map( (discount) => discount.price * 0.9);
+console.log(discounts);
+
+//filter
+
+const limit = gadgets
+        .filter((limits)=> limits.price>=10000)
+        .sort((a,b)=> a.price-b.price);
+
+console.log(limit);
+
+//reduce
+
+const calulateTotal = gadgets
+            .reduce((sum,product) => sum+product.price,0);
+console.log(calulateTotal);
+
