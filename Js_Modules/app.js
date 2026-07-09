5f1044903a2528f17f74afdfec692d26e33b3702
SriVarshininoothi <noothi.srivarshini982@gmail.com>
2026-07-09 17:46:07 +0530

    updated

--- Js_Modules/app.js
+++ Js_Modules/app.js
@@ -1,7 +1,12 @@
 
-import {add,substract,mul,division} from "./Math.js";
+import {add,subtract,mul,division} from "./Math.js";
 
 console.log(add(2,3));
-console.log(substract(8,7));
+console.log(subtract(8,7));
 console.log(mul(4,3));
 console.log(division(66,3));
+
+
+import greet from "./Math.js";
+
+console.log(greet("Sri"));
